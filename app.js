881cdf19b27e2050caae8a744eb242ab249af2a6
Fasih Ul Haq <fasihulhaq245@gmail.com>
2024-09-28 08:27:05 +0500

    Finalized

--- app.js
+++ app.js
@@ -62,9 +62,11 @@ fetch('https://jsonplaceholder.typicode.com/users')
     function updatedDisplay(){
         if (index<res.length){
     div.innerHTML =
-    `<h1>Employee Details</h1>
+    `<fieldset >
+    <legend> <h1 class="text-4xl">Employee Details</h1> </legend>
     <b>Name: </b> ${res[index].name} <br> <b>User Name:</b> ${res[index].username} <br> <b>Phone:</b> ${res[index].phone} 
     <br> <b>Email:</b> ${res[index].email} <br>
+    </fieldset>
     <button onclick="decreaseIndex()">Previous</button>
     <button onclick="increaseIndex()">Next</button>
     `;}
